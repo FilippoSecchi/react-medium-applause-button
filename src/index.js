3b802dfc07e9901346fbbed3bdfee8da9a54f825
Filippo Secchi <69622678+FilippoSecchi@users.noreply.github.com>
2020-12-02 06:28:44 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import ApplauseButton from "./ApplauseButton";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <React.StrictMode>
+    <ApplauseButton />
+  </React.StrictMode>,
+  rootElement
+);
